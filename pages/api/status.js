385fd4e4e3babadf0877de0139c0796995e45ac4
Gustavo Orlandini <guorlandini@outlook.com>
2026-03-11 17:08:28 -0300

    Testando novamentemente API

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,7 +1,6 @@
 // endpoint para mostrar o status do sistema. request trata do que entra na função e o response trata do quero responder
 function status(request, response) {
-  // o objeto response possui um método chamado status, que define o status code da resposta, onde por convensão o 200 é o statuscode para tudo OK
-  response.status(200).send("tudo OK");
+  response.status(200).json({ chave: "valor" });
 }
 
 export default status;
