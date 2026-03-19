b9f43395afc9d6d8feb12d60c1785e2be5c5def0
Gustavo Orlandini <guorlandini@outlook.com>
2026-03-19 17:53:42 -0300

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString(); // date é um objeto com a data atual, é necessário o toISOString para que seja mostrado a data no formato desejado
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnections.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseUsedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseUsedConnectionsValue = databaseUsedConnections.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        used_connections: databaseUsedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
